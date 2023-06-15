6a8e9eab5876db3153a6ebc1439e3573adde5647
OwenSyKim <owen.sykim@gmail.com>
2023-06-14 23:29:33 -0700

    Setup Routes & Add pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import MovieDetail from './pages/MovieDetail';
+import Home from './pages/Home';
+import Movies from './pages/Movies';
 
 function App() {
   return (
     <div>
-
+      <Routes>
+        <Route path="/" element={<Home/>}/> 
+        <Route path="/movies" element={<Movies/>}/> 
+        <Route path="/:id" element={<MovieDetail/>}/> 
+      </Routes>
     </div>
   );
 }
